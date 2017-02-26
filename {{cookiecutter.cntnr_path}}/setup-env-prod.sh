f6ef84c791c0878eb390f62d177bc82f2be7594b
Mike Lyons <mikejlyons@gmail.com>
2017-02-26 13:49:20 -0500

    Make sure PROJECT_DEV isn't set for prod

--- {{cookiecutter.cntnr_path}}/setup-env-prod.sh
+++ {{cookiecutter.cntnr_path}}/setup-env-prod.sh
@@ -11,4 +11,7 @@ export {{cookiecutter.envvar_prefix|upper}}_MACHINE="{{cookiecutter.machine_pref
 export {{cookiecutter.envvar_prefix|upper}}_MACHINE_DRIVER="--driver {{cookiecutter.prod_docker_driver}} --generic-ip-address ${{cookiecutter.envvar_prefix|upper}}_PROD_IP --generic-ssh-user ${{cookiecutter.envvar_prefix|upper}}_PROD_USER --generic-ssh-key ${{cookiecutter.envvar_prefix|upper}}_SSH_PRIVATE_KEY"
 export {{cookiecutter.envvar_prefix|upper}}_DOCKER_COMPOSE_OVERRIDE_FILENAME="docker-compose-production.yml"
 export {{cookiecutter.envvar_prefix|upper}}_MACHINE_ENGINE_OPT="--engine-opt=\"bip={{cookiecutter.bridge_ip_cidr}}\""
+
+unset {{cookiecutter.envvar_prefix|upper}}_DEV
+
 . $(dirname $BASH_SOURCE)/setup-env-common.sh
