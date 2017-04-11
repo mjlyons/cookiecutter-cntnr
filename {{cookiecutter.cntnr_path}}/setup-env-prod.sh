dd245479110424acdd9d424748ea5015fdb7eed8
Mike Lyons <mikejlyons@gmail.com>
2017-04-10 23:28:46 -0400

    Remove DO fingerprint

--- {{cookiecutter.cntnr_path}}/setup-env-prod.sh
+++ {{cookiecutter.cntnr_path}}/setup-env-prod.sh
@@ -8,7 +8,6 @@ export {{cookiecutter.envvar_prefix|upper}}_SSH_PRIVATE_KEY="{{cookiecutter.prod
 
 export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_ACCESS_TOKEN="{{cookiecutter.digitalocean_access_token}}"
 export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_SIZE="{{cookiecutter.digitalocean_size}}"
-export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_SSH_KEY_FINGERPRINT="{{cookiecutter.digitalocean_ssh_key_fingerprint}}"
 export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_IPV6="{{cookiecutter.digitalocean_ipv6}}"
 export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_PRIVATE_NET="{{cookiecutter.digitalocean_private_net}}"
 export {{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_BACKUPS="{{cookiecutter.digitalocean_backups}}"
@@ -19,7 +18,7 @@ export {{cookiecutter.envvar_prefix|upper}}_DOCKER_COMPOSE_OVERRIDE_FILENAME="do
 export {{cookiecutter.envvar_prefix|upper}}_MACHINE_ENGINE_OPT="--engine-opt=\"bip={{cookiecutter.bridge_ip_cidr}}\""
 
 if [ "{{cookiecutter.prod_docker_driver}}" = "digitalocean" ]; then
-  export {{cookiecutter.envvar_prefix|upper}}_MACHINE_DRIVER="--driver digitalocean --digitalocean-access-token=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_ACCESS_TOKEN --digitalocean-size=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_SIZE --digitalocean-ipv6=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_IPV6 --digitalocean-backups=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_BACKUPS --digitalocean-private-networking=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_PRIVATE_NET --digitalocean-ssh-key-fingerprint=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_SSH_KEY_FINGERPRINT"
+  export {{cookiecutter.envvar_prefix|upper}}_MACHINE_DRIVER="--driver digitalocean --digitalocean-access-token=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_ACCESS_TOKEN --digitalocean-size=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_SIZE --digitalocean-ipv6=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_IPV6 --digitalocean-backups=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_BACKUPS --digitalocean-private-networking=${{cookiecutter.envvar_prefix|upper}}_DIGITALOCEAN_PRIVATE_NET"
 elif [ "{{cookiecutter.prod_docker_driver}}" = "generic" ]; then
   export {{cookiecutter.envvar_prefix|upper}}_MACHINE_DRIVER="--driver {{cookiecutter.prod_docker_driver}} --generic-ip-address ${{cookiecutter.envvar_prefix|upper}}_PROD_IP --generic-ssh-user ${{cookiecutter.envvar_prefix|upper}}_PROD_USER --generic-ssh-key ${{cookiecutter.envvar_prefix|upper}}_SSH_PRIVATE_KEY"
 else
